aae6a4772b59f97de7f1fea2c24c74d85a1ce444
sergeantg <sergeantg@live.com>
2015-03-27 15:12:30 +0800

    first

---
+++ app/src/main/java/com/zujia/android/zujia/activity/custom/CustomMainActivity.java
@@ -0,0 +1,105 @@
+package com.zujia.android.zujia.activity.custom;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TabHost;
+
+import com.zujia.android.zujia.activity.LoginActivity;
+import com.zujia.android.zujia.activity.SettingActivity;
+import com.zujia.android.zujia.R;
+
+
+public class CustomMainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_main);
+
+        // 获取TabHost对象
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+        // 如果没有继承TabActivity时，通过该种方法加载启动tabHost
+        tabHost.setup();
+
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("消息", getResources().getDrawable(R.drawable.message)).setContent(
+                R.id.tab1));
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("联系记录", getResources().getDrawable(R.drawable.contact)).setContent(
+                R.id.tab2));
+        tabHost.addTab(tabHost.newTabSpec("tab1")
+                .setIndicator("租家帮", getResources().getDrawable(R.drawable.zujiabang)).setContent(
+                R.id.tab3));
+        //tabHost.addTab(tabHost.newTabSpec("tab1")
+        //        .setIndicator("我", getResources().getDrawable(R.drawable.me)).setContent(
+        //        R.id.tab4));
+
+        android.view.View view = getLayoutInflater().inflate(R.layout.custom_tab_me, null);
+
+        tabHost.addTab(tabHost.newTabSpec("tab1").setIndicator(view).setContent(
+                        R.id.tab4));
+        //tabHost.getTabWidget().getChildAt(3).setBackgroundResource(R.drawable.tab_me_selector);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_custom_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        //noinspection SimplifiableIfStatement
+        switch (item.getItemId()){
+            case R.id.searchItem:
+                startActivity(new Intent().setClass(this, LoginActivity.class));
+            break;
+        }
+
+        ;
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //my tab click methods
+    public void growthClick(View v){
+
+    }
+    public void postWantClick(View v){
+        startActivity(new Intent().setClass(this, CustomPostWantActivity.class));
+    }
+    public void settingClick(View v){
+        startActivity(new Intent().setClass(this, SettingActivity.class));
+    }
+    public void feedbackClick(View v){
+        startActivity(new Intent().setClass(this, CustomFeedbackActivity.class));
+    }
+    public void  aboutClick(View v){
+        startActivity(new Intent().setClass(this, AboutActivity.class));
+    }
+    public void logoutClick(View v){
+    }
+
+    //discovery tab click methods
+    public void moveClick(View v){
+
+    }
+    public void housekeeperClick(View v){
+
+    }
+    public void otherClick(View v){
+
+    }
+
+}
+
---
+++ app/src/main/java/com/zujia/android/zujia/activity/custom/HousesMapActivity.java
@@ -0,0 +1,41 @@
+package com.zujia.android.zujia.activity.custom;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.zujia.android.zujia.R;
+
+
+public class HousesMapActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_houses_map);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_houses_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
