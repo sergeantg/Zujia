14899e5e6452082807d719860d6e737cdd6e7a39
sergeantg <sergeantg2@163.com>
2015-04-14 20:19:13 +0800

    试图修改fragment

--- app/src/main/java/com/zujia/android/zujia/ui/ZujiabangFragment.java
+++ app/src/main/java/com/zujia/android/zujia/ui/ZujiabangFragment.java
@@ -1,9 +1,9 @@
 package com.zujia.android.zujia.ui;
 
 import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,32 +11,12 @@ import android.view.ViewGroup;
 import com.zujia.android.zujia.R;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ZujiabangFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ZujiabangFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class ZujiabangFragment extends Fragment {
+public class ZujiabangFragment extends Fragment implements View.OnClickListener {
 
     private View [] vv = new View[10];//visability view
     private View [] vc = new View[5];//color view
-    private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ZujiabangFragment newInstance(String param1, String param2) {
-        ZujiabangFragment fragment = new ZujiabangFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public ZujiabangFragment() {
         // Required empty public constructor
@@ -45,10 +25,6 @@ public class ZujiabangFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -74,43 +50,91 @@ public class ZujiabangFragment extends Fragment {
         return temp;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+    }
+    //discovery tab click methods
+    public void menuClick(View v){
+
+        int id = v.getId();
+        int type = 0;
+        int color = 0;
+        boolean open = false;
+
+        FragmentTransaction transaction =
+                getSupportFragmentManager().beginTransaction();
+
+        if(lastPressedItem == id){//隐藏fragment,显示vv,重置lastPressedItem
+            color = getResources().getColor(R.color.white);
+            lastPressedItem = -1;
+            open = false;
+        }else{
+            switch (id){
+                case R.id.layout_rent:
+                    type = 0;
+                    color = getResources().getColor(R.color.color1);
+                    break;
+                case R.id.layout_live:
+                    type = 1;
+                    color = getResources().getColor(R.color.color2);
+                    break;
+                case R.id.layout_appliance:
+                    type = 2;
+                    color = getResources().getColor(R.color.color3);
+                    break;
+                case R.id.layout_furniture:
+                    type = 3;
+                    color = getResources().getColor(R.color.color4);
+                    break;
+                case R.id.layout_life:
+                    type = 4;
+                    color = getResources().getColor(R.color.color5);
+                    break;
+            }
+            open = true;
+            lastPressedItem = id;
+            ServiceSecondMenuItemFragment f =
+                    ServiceSecondMenuItemFragment.newInstance(type, color);
+            transaction.replace(R.id.menu_container, f);
+            transaction.commit();
+        }
+
+        toggleVisiable(open, type, color);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
+    @Override
+    public void onClick(View v) {
+
     }
 
+    private void toggleVisiable(boolean open, int p, int color){
+        if(open){
+            for(View i:vv) {
+                i.setVisibility(View.GONE);
+            }
+            titleLeftBtn.setVisibility(View.VISIBLE);
+            fl.setVisibility(View.VISIBLE);
+            //设置颜色
+            for(View i:vc){
+                i.setBackgroundColor(0xffffffff);
+            }
+            vc[p].setBackgroundColor(color);
+        }else{
+            for(View i:vv) {
+                i.setVisibility(View.VISIBLE);
+            }
+            titleLeftBtn.setVisibility(View.GONE);
+            fl.setVisibility(View.GONE);
+            for(View i:vc){
+                i.setBackgroundColor(0xffffffff);
+            }
+        }
+    }
 }
--- app/src/main/java/com/zujia/android/zujia/ui/custom/CustomMeFragment.java
+++ app/src/main/java/com/zujia/android/zujia/ui/custom/CustomMeFragment.java
@@ -1,10 +1,9 @@
 package com.zujia.android.zujia.ui.custom;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,42 +14,9 @@ import com.zujia.android.zujia.ui.LoginActivity;
 import com.zujia.android.zujia.ui.SettingActivity;
 
 /**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link CustomMeFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link CustomMeFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
-public class CustomMeFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class CustomMeFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CustomMeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CustomMeFragment newInstance(String param1, String param2) {
-        CustomMeFragment fragment = new CustomMeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     public CustomMeFragment() {
         // Required empty public constructor
@@ -60,76 +26,55 @@ public class CustomMeFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_custom_me, container, false);
-    }
+        View temp = inflater.inflate(R.layout.fragment_custom_me, container, false);
+        temp.findViewById(R.id.growth).setOnClickListener(this);
+        temp.findViewById(R.id.postWant).setOnClickListener(this);
+        temp.findViewById(R.id.logout).setOnClickListener(this);
+        temp.findViewById(R.id.setting).setOnClickListener(this);
+        temp.findViewById(R.id.feedback).setOnClickListener(this);
+        temp.findViewById(R.id.about).setOnClickListener(this);
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        return temp;
     }
 
     @Override
     public void onAttach(Activity activity) {
         super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        public void onFragmentInteraction(Uri uri);
-    }
-
-    //my tab click methods
-    public void growthClick(View v){
-
-    }
-    public void postWantClick(View v){
-        startActivity(new Intent().setClass(this, CustomPostWantActivity.class));
-    }
-    public void settingClick(View v){
-        startActivity(new Intent().setClass(this, SettingActivity.class));
-    }
-    public void feedbackClick(View v){
-        startActivity(new Intent().setClass(this, CustomFeedbackActivity.class));
-    }
-    public void  aboutClick(View v){
-        startActivity(new Intent().setClass(this, AboutActivity.class));
-    }
-    public void logoutClick(View v){
-        ((AppContext)(getActivity().getApplication())).logout();
-        startActivity(new Intent().setClass(this, LoginActivity.class));
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.growth:
+                break;
+            case R.id.postWant:
+                startActivity(new Intent().setClass(getActivity(), CustomPostWantActivity.class));
+                break;
+            case R.id.logout:
+                ((AppContext)(getActivity().getApplication())).logout();
+                startActivity(new Intent().setClass(getActivity(), LoginActivity.class));
+                break;
+            case R.id.setting:
+                startActivity(new Intent().setClass(getActivity(), SettingActivity.class));
+                break;
+            case R.id.feedback:
+                startActivity(new Intent().setClass(getActivity(), CustomFeedbackActivity.class));
+                break;
+            case R.id.about:
+                startActivity(new Intent().setClass(getActivity(), AboutActivity.class));
+                break;
+        }
     }
 
 }
